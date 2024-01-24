0ba0a27a9037ecfcd1884fe9164515380ac864ff
Hiram Artnak Martins <hiram.artnak@gmail.com>
2024-01-23 21:01:28 -0300

    remove duplicate files

--- ses/backend/api.py
+++
@@ -1,8 +0,0 @@
-from fastapi import FastAPI
-from .routes.course import router as course_router
-
-
-def create_app()->FastAPI:
-    app = FastAPI()
-    app.include_router(course_router)
-    return app
