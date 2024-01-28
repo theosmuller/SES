27732b5df303cffbf146a1301f767f5e1f23c6d4
Théo Santiago Müller <theosmuller@gmail.com>
2024-01-28 11:50:21 -0300

    adding add class menu, needs lots of fixes + functionality

--- frontend/ses/src/views/AdminPage.vue
+++ frontend/ses/src/views/AdminPage.vue
@@ -1,118 +1,128 @@
 <template>
     <div class="header">
-        <div class="header-item">ADMIN TOOLS</div>
-        <div class="header-item right">{{ collegeCourseName }}</div>
-        <div class="header-item">
-            <button class="exit-button" @click="exitAdminTools">
-                <i class="pi pi-sign-out"></i> EXIT
-            </button>
-        </div>
+      <div class="header-item">ADMIN TOOLS</div>
+      <div class="header-item right">{{ collegeCourseName }}</div>
+      <div class="header-item">
+        <button class="exit-button" @click="exitAdminTools">
+          <i class="pi pi-sign-out"></i> EXIT
+        </button>
+      </div>
     </div>
     <div class="admin-page">
-        <div class="side-menu">
-            <div class="menu-item" :class="{ 'selected': isSearchEditClassSelected }"
-                @click="toggleButton('search-edit-class')" data-button="search-edit-class">
-                <i class="pi pi-search"></i>
-                SEARCH/EDIT CLASS
-            </div>
-            <div class="menu-item" :class="{ 'selected': isAddClassSelected }" @click="toggleButton('add-class')"
-                data-button="add-class">
-                <i class="pi pi-plus-circle"></i>
-                ADD CLASS
-            </div>
-            <div class="menu-item" :class="{ 'selected': isSetEnrollmentDatesSelected }"
-                @click="toggleButton('set-enrollment-dates')" data-button="set-enrollment-dates">
-                <i class="pi pi-calendar"></i>
-                SET ENROLLMENT DATES
-            </div>
-            <div class="menu-item disabled" :class="{ 'selected': isSetupUniversitySelected }"
-                data-button="setup-university">
-                <i class="pi pi-cog"></i>
-                SETUP UNIVERSITY AND COURSE
-            </div>
+      <div class="side-menu">
+        <div
+          class="menu-item"
+          :class="{ 'selected': isSearchEditClassSelected }"
+          @click="toggleButton('search-edit-class')"
+          data-button="search-edit-class"
+        >
+          <i class="pi pi-search"></i> SEARCH/EDIT CLASS
         </div>
-        <div class="main-content">
+        <div
+          class="menu-item"
+          :class="{ 'selected': isAddClassSelected }"
+          @click="toggleButton('add-class')"
+          data-button="add-class"
+        >
+          <i class="pi pi-plus-circle"></i> ADD CLASS
         </div>
+        <div
+          class="menu-item"
+          :class="{ 'selected': isSetEnrollmentDatesSelected }"
+          @click="toggleButton('set-enrollment-dates')"
+          data-button="set-enrollment-dates"
+        >
+          <i class="pi pi-calendar"></i> SET ENROLLMENT DATES
+        </div>
+        <div
+          class="menu-item disabled"
+          :class="{ 'selected': isSetupUniversitySelected }"
+          data-button="setup-university"
+        >
+          <i class="pi pi-cog"></i> SETUP UNIVERSITY AND COURSE
+        </div>
+      </div>
+      <transition name="fade" mode="out-in">
+        <div class="main-content" :key="isAddClassSelected">
+          <div v-if="isAddClassSelected">
+            <div class="add-class-menu">
+              <AddClassMenu />
+            </div>
+          </div>
+        </div>
+      </transition>
     </div>
-</template>
+  </template>
   
-<script setup lang="ts">
-import { ref, watch } from 'vue';
-import { useRouter } from 'vue-router';
-
-const router = useRouter();
-
-const collegeCourseName = ref("UFRGS - Ciência da Computação"); 
-
-const buttonActions = {
-    'search-edit-class': () => {
-
-    },
-    'add-class': () => {
-    
-    },
-    'set-enrollment-dates': () => {
-
-    },
-    'setup-university': () => {
-
-    },
-};
-
-const selectedButton = ref('');
-
-const toggleButton = (button: string) => {
+  <script setup lang="ts">
+  import { ref, watch } from 'vue';
+  import { useRouter } from 'vue-router';
+  import AddClassMenu from '../components/AddClassMenu.vue';
+  
+  const router = useRouter();
+  const collegeCourseName = ref('UFRGS - Ciência da Computação');
+  const selectedButton = ref('');
+  
+  const isAddClassSelected = ref(false);
+  
+  const toggleButton = (button: string) => {
     selectedButton.value = button;
-
-    if (buttonActions[selectedButton.value]) {
-        buttonActions[selectedButton.value]();
+  
+    if (button === 'add-class') {
+      isAddClassSelected.value = true;
+    } else {
+      isAddClassSelected.value = false;
     }
-};
-
-watch(selectedButton, (newValue) => {
+  
+    if (buttonActions[selectedButton.value]) {
+      buttonActions[selectedButton.value]();
+    }
+  };
+  
+  watch(selectedButton, (newValue) => {
     resetSelectedClasses();
     const el = document.querySelector(`.menu-item[data-button="${newValue}"]`);
     if (el) {
-        el.classList.add('selected');
+      el.classList.add('selected');
     }
-});
-
-const resetSelectedClasses = () => {
+  });
+  
+  const resetSelectedClasses = () => {
     const menuItems = document.querySelectorAll('.menu-item');
     menuItems.forEach((item) => {
-        item.classList.remove('selected');
+      item.classList.remove('selected');
     });
-};
-
-const exitAdminTools = () => {
-    router.push('/login');
-};
-</script>
+  };
   
-<style scoped>
-.admin-page {
+  const exitAdminTools = () => {
+    router.push('/login');
+  };
+  </script>
+  
+  <style scoped>
+  .admin-page {
     display: flex;
     justify-content: center;
-    align-items: center;
     height: 95vh;
     width: 100vw;
     padding: 50px;
     background-color: #0e0720;
     background-size: repeat;
     background-position: center;
-}
-
-.side-menu {
-    width: 300px;
+  }
+  
+  .side-menu {
+    width: fit-content;
+    height: fit-content;
     background-color: #000000;
     color: #fff;
     padding: 20px;
     display: flex;
     flex-direction: column;
     border-radius: 10px;
-}
-
-.menu-item {
+  }
+  
+  .menu-item {
     cursor: pointer;
     padding: 20px;
     background-color: #3a0754;
@@ -122,31 +132,31 @@ const exitAdminTools = () => {
     box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.15);
     border-radius: 10px;
     transition: background-color 0.3s ease;
-}
-
-.menu-item:hover {
+  }
+  
+  .menu-item:hover {
     background-color: #4a156b;
-}
-
-.menu-item i {
+  }
+  
+  .menu-item i {
     margin-right: 10px;
-}
-
-.menu-item.selected {
+  }
+  
+  .menu-item.selected {
     background-color: #e371ff;
-}
-
-.menu-item.disabled {
+  }
+  
+  .menu-item.disabled {
     opacity: 0.6;
     cursor: not-allowed;
-}
-
-.main-content {
+  }
+  
+  .main-content {
     flex: 1;
     padding: 20px;
-}
-
-.header {
+  }
+  
+  .header {
     display: flex;
     justify-content: space-between;
     align-items: center;
@@ -155,35 +165,44 @@ const exitAdminTools = () => {
     color: #fff;
     height: 5vw;
     width: 100vw;
-}
-
-.header-item {
+  }
+  
+  .header-item {
     font-size: 42px;
     font-family: 'Roboto', sans-serif;
-}
-
-.header-item.right {
+  }
+  
+  .header-item.right {
     text-align: right;
-}
-
-.exit-button {
-  background: none;
-  border: none;
-  border-radius: 10px;
-  cursor: pointer;
-  color: #fff;
-  font-size: 36px;
-  font-family: 'Roboto', sans-serif;
-  padding: 10px;
-  transition: background-color 0.3s ease;
-}
-
-.exit-button:hover {
-  background-color: #4a156b; 
-}
-
-.exit-button i {
-  font-size: 36px; 
-}
-</style>
+  }
+  
+  .exit-button {
+    background: none;
+    border: none;
+    border-radius: 10px;
+    cursor: pointer;
+    color: #fff;
+    font-size: 36px;
+    font-family: 'Roboto', sans-serif;
+    padding: 10px;
+    transition: background-color 0.3s ease;
+  }
+  
+  .exit-button:hover {
+    background-color: #4a156b;
+  }
+  
+  .exit-button i {
+    font-size: 36px;
+  }
+  
+  .fade-enter-active,
+  .fade-leave-active {
+    transition: opacity 0.15s;
+  }
+  
+  .fade-enter, .fade-leave-to {
+    opacity: 0;
+  }
+  </style>
   
