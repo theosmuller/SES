a976b135c4e977ed94a9d110ed9729a48c439ed5
Hiram Artnak Martins <hiram.artnak@gmail.com>
2024-01-29 00:33:27 -0300

    routers

--- new_backend/ses_backend/api/routes/__init__.py
+++ new_backend/ses_backend/api/routes/__init__.py
@@ -1,3 +1,8 @@
 from ses_backend.api.routes.university import router as university_router
+from ses_backend.api.routes.course import router as course_router
+from ses_backend.api.routes.professor import router as professor_router
+from ses_backend.api.routes.student import router as student_router
+from ses_backend.api.routes.program import router as program_router
 
-routers = [university_router]
+
+routers = [university_router, course_router, professor_router, student_router, program_router]
